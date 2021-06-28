ffed9221c15db9d433b15d6c29203590e6de5ea3
Wieneke-88 <84339361+Wieneke-88@users.noreply.github.com>
2021-06-28 22:25:40 +0200

    Add files via upload
    1. Het loggen van variabelen
    2. typeOf gebruiken om een indicatie te geven van welk type primitive type je gebruik maakt - string, number, boolean, undefined, null 
    3. Begrepen en afgerond

---
+++ index.js
@@ -0,0 +1,63 @@
+//INHOUD; LOG VARIABELEN 
+//DEZE BEGRIJP IK! 
+
+console.log("Hello Winc Academy");
+VM177: 1 Hello Winc Academy
+undefined
+let name = "Wieneke";
+undefined
+console.log(name);
+VM280: 1 Wieneke
+undefined
+//Je kunt geen variabele aanmaken met 'if' als keyword, want dit is een reserved keyword 
+let if = 2
+VM305: 1 Uncaught SyntaxError: Unexpected token 'if'
+4 + 4;
+8
+console.log(4 + 4);
+VM440: 1 8
+undefined
+"4" + "4";
+"44"
+console.log("4" + "4");
+VM531: 1 44
+undefined
+let sum = 3 + 4;
+undefined
+console.log(sum);
+VM617: 1 7
+undefined
+let subtraction = 8 - 3;
+undefined
+console.log(subtraction);
+VM795: 1 5
+undefined
+let multiplication = 5 * 3;
+undefined
+console.log(multiplication);
+VM905: 1 15
+undefined
+let division = 5 / 2;
+undefined
+console.log(division);
+VM1008: 1 2.5
+undefined
+let modulus = 12 % 5;
+undefined
+console.log(modulus);
+VM1113: 1 2
+undefined
+let age = 33;
+undefined
+typeof age
+"number"
+console.log(typeof age);
+VM1258: 1 number
+undefined
+let age = "33";
+undefined
+typeof age
+"string"
+console.log(typeof age);
+VM1396: 1 string
+undefined
